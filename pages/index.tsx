f585364f4faac82dd9f0cc8a262dce2d2e7e285a
Kaayzeer~ <mautiagiro@gmail.com>
2022-09-06 19:53:47 +0200

    installed needed packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  color: red;
+`;
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return <StyledDiv>hello</StyledDiv>;
 };
 
 export default Home;
