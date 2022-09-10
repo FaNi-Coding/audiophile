cc57239ee2272cc3e7adc452871d514b184acb91
Kaayzeer~ <mautiagiro@gmail.com>
2022-09-10 13:38:05 +0200

    added themeprovider with global themes and updated the button component to more dynamically

---
+++ styles/theme.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  colors: {
+    header: "#0E0E0E",
+    body: "#FAFAFA",
+    footer: "#101010",
+
+    primary: "#ffffff",
+    secondary: "#00000080",
+    outlined: "#000000",
+  },
+
+  button: {
+    primary: "#D87D4A",
+    secondary: "#ffffff",
+    outlined: "#ffffff",
+  },
+
+  fonts: {
+    fontBold: "700",
+    fontNormal: "500",
+  },
+};
