33b4f52079c39a198413d481ea871754da41601d
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-17 11:23:28 +0200

    adding attributes to button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import type { NextPage } from "next";
-import styled from "styled-components";
 import Layout from "../components/Layout/Layout";
 import Button from "../components/UI-Components/Buttons/Button";
 import Input from "../components/UI-Components/Inputs/Input";
 
-const StyledDiv = styled.div`
-  color: red;
-`;
 
 const Home: NextPage = () => {
+
+
   return (
     <Layout title={""} description={""} keywords={""}>
-      <Button primary>SEE PRODUCT</Button>
-      <Button outlined>SEE PRODUCT</Button>
-      <Button secondary>SHOP</Button>
+      <Button href={{
+        pathname:'/category/1',
+        query: {id: 100,
+        price: 45
+      }
+    }} primary>SEE PRODUCT</Button>
+      <Button href="yoo" outlined>SEE PRODUCT</Button>
+      <Button href="yoo" secondary>SHOP</Button>
       <Input name="Name" type="email" />
       <Input type="radio" value="e-Money" />
     </Layout>
