3c853866ab767aff320b891ca1288dca993a48ee
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-17 20:03:40 +0200

    added logo to nav

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,12 +9,17 @@ const Navbar = (props: Props) => {
     <StyledNav className='flex'>
       <nav className="container flex align-center justify-around">
         <div className='menu'>
-          <Image src="/assets/shared/tablet/icon-hamburger.svg" alt="hamburger-icon" width={16}
+          <Image src="/assets/shared/tablet/icon-hamburger.svg" alt="hamburger" width={16}
           height={15}
           />
         </div>
+        <div className='logo'>
+          <Image src="/assets/shared/desktop/logo.svg" alt="logo" width={143}
+          height={25}
+          />
+        </div>
         <div className='cart'>
-          <Image src='/assets/shared/desktop/icon-cart.svg' alt='cart-icon' width={23}
+          <Image src='/assets/shared/desktop/icon-cart.svg' alt='cart' width={23}
           height={20}/>
         </div>
       </nav>
