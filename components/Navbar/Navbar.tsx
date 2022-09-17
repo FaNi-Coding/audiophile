c7b83c364ee1eb46df8e27b26105b57d715962bf
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-17 20:01:21 +0200

    Fixed navbar styling and component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { StyledNav } from './NavbarStyles'
+import { useMediaQuery } from '../../hooks/useMediaQuery'
+import Image from 'next/image'
 
-type Props = {};
+type Props = {}
 
-export default function Navbar({}: Props) {
-  return <div>Navbar</div>;
+const Navbar = (props: Props) => {
+  return (
+    <StyledNav className='flex'>
+      <nav className="container flex align-center justify-around">
+        <div className='menu'>
+          <Image src="/assets/shared/tablet/icon-hamburger.svg" alt="hamburger-icon" width={16}
+          height={15}
+          />
+        </div>
+        <div className='cart'>
+          <Image src='/assets/shared/desktop/icon-cart.svg' alt='cart-icon' width={23}
+          height={20}/>
+        </div>
+      </nav>
+    </StyledNav>
+  )
 }
+
+export default Navbar
---
+++ components/Navbar/NavbarStyles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const StyledNav = styled.header`
+
+  .align-center {
+    align-items: center;
+  }
+
+  .justify-around {
+    justify-content: space-around
+  }
+
+  h-full {
+    height: 100%;
+  }
+`
