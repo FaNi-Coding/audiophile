c57ac3742f790f6b69531ec60c9232199b56dbca
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-17 11:26:20 +0200

    adding params for function

--- components/UI-Components/Buttons/Button.tsx
+++ components/UI-Components/Buttons/Button.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { DefaultButton } from "./ButtonStyle";
-import type { ButtonProps } from "./ButtonInterface";
+import type { ButtonPropsInterface } from "./ButtonInterface";
 import Link from "next/link";
 
 /**
@@ -8,11 +8,16 @@ import Link from "next/link";
  * @param {Boolean} primary   - Default button
  * @param {Boolean} outlined  - Outlined button
  * @param {Boolean} secondary - Text with icon to the right
+ * @param {String}  href      - href = "product"
+ * @param {Object}  href      - href = { pathname: 'category',
+ *                                       query: { product: 'EaryPhones' }
+ *                                     }
  */
-const Button: NextPage<ButtonProps> = ({ className, children, ...props }) => {
+const Button = ({ className, children, ...props }:ButtonPropsInterface) => {
+  
   return (
     <DefaultButton className={className} {...props}>
-      <Link href={props.url!}>{children}</Link>
+      <Link href={props.href!}><a>{children}</a></Link>
     </DefaultButton>
   );
 };
