2f9d6f56afdcb8efb84d83256d5c2b5a235cc4f4
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-17 13:00:37 +0200

    Helper added

---
+++ helpers/boilerplate/functions.ts
@@ -0,0 +1,18 @@
+ import {MouseEvent} from 'react';
+
+ 
+// Click handler for the button
+export const handleClick = (e:MouseEvent<HTMLButtonElement>) => {
+     e.preventDefault()
+     console.log(e)
+}
+
+export const HrefBoiler = {
+     pathname:'/category/1',
+     query: {  
+          id: 1,
+          price: 100,
+          currency: 'SEK',
+          product: 'EarPhones'
+     }
+}
