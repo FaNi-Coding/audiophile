a9ca9d6ca0098a509566bd55df07b516ada00ddb
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-17 11:21:25 +0200

    testing slug page for category

---
+++ pages/category/[slug].tsx
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+import {useRouter} from 'next/router';
+export interface ICategoryProps {
+}
+
+const Category = (props: ICategoryProps) => {
+     const router = useRouter();
+     const [id, setId] = useState(router.query.id !== undefined ? router.query.id : 12);
+  return (
+    <div>
+      <h1>Hej {id&& router.query.id} </h1>
+    </div>
+  );
+}
+
+export default Category;
