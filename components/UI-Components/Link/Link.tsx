e36ed55f504d31ddf3528ace84991f11f5a80609
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-17 12:10:36 +0200

    Creating LinkUI component

---
+++ components/UI-Components/Link/Link.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+type Props = {
+  href?: string | Object
+  text?: string 
+}
+
+const LinkUI = ({ href, text="linkText" }: Props) => {
+  return (
+    <Link href={href!}><a>{text}</a></Link> 
+  )
+}
+
+export default LinkUI
