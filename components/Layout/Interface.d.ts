1ccabe323ae2112cdd25031a2329df83e2190c62
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-09-18 20:14:27 +0200

    Layout interface

---
+++ components/Layout/Interface.d.ts
@@ -0,0 +1,6 @@
+export type LayoutProps = {
+     children: React.ReactNode;
+     title: string | "Audiophile | texchnology";
+     description: string | "alot of technologies";
+     keywords: string | "tech, headphones, earphones, speakers";
+   }
