3fad7e97f910a20738b5c6edc41820e6fe33ada4
robonexx <robertwagar@gmail.com>
2022-11-02 21:06:12 +0100

    kommentar

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import LinkUI from "../components/UI-Components/Link/Link";
 import ShopLinks from "../components/UI-Components/Sections/ShopLinks";
 import { handleClick, HrefBoiler } from "../helpers/boilerplate/functions";
 
-/* testing 1234 */
+/* Ok ska börja jobba sluta lalla :D tack fadil fantastiks feedback */
 
 const Home: NextPage = () => {
   return (
