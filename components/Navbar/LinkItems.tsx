210f71f93d7df24367a4dcd0ef92acd8252624b0
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2022-11-12 11:29:13 +0100

    Fragment added

--- components/Navbar/LinkItems.tsx
+++ components/Navbar/LinkItems.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Button from "../UI-Components/Buttons/Button";
 
 type Props = {
-  array: any;
+  array: any
   mobile?: boolean;
   menuItems?: boolean;
   components?: any;
@@ -16,7 +16,9 @@ const LinkItems = ({ array, menuItems }: Props) => {
       <li key={idx}>
         {menuItems && <img src={item.img} alt={item.name} />}
         <Link href={item.href}>
-          <a>{item.name}</a>
+          <>
+            <a>{item.name}</a>
+          </>
         </Link>
         {menuItems && <Button secondary>shop</Button>}
       </li>
